548f54c0640e0c84cd3585c3fb5f71c6ca71babb
yakarmurat@yahoo.com <abuzerbuldozer@hotmail.com>
2018-03-22 07:27:09 +0100

    safari message fix

--- webapp/src/js/global.js
+++ webapp/src/js/global.js
@@ -10,6 +10,17 @@
       } ).done(function() {
     	  //$.i18n().locale = 'de';
     	  $('body').i18n();
+    	  
+    	  var isMobileDevice = !!(/Android|webOS|iPhone|iPad|iPod|BB10|BlackBerry|IEMobile|Opera Mini|Mobile|mobile/i.test(navigator.userAgent || ''));
+
+    	  var ua = window.navigator.userAgent;
+    	  var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+    	  var webkit = !!ua.match(/WebKit/i);
+    	  var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+    	  
+    	  if( !(isMobileDevice && !iOSSafari) ){
+    			showDialogMessage( $.i18n('msgtype-error'), $.i18n('error-mobilsafari') ,true );
+    		}
 //        set_locale_to(url('?locale'));
 // 
 //        History.Adapter.bind(window, 'statechange', function(){
